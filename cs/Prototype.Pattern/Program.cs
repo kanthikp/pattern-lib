1cff96e0845643074fbbd5d0c33df07bd02c8b40
santiaago <santiago.ariassar@gmail.com>
2013-04-08 14:17:41 +0200

    Prototype pattern part 1

---
+++ cs/Prototype.Pattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype.Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var digger = new WebPageDigger("http://www.google.com");
+            digger.PrintPageData();
+
+            var digger2 = digger.Clone() as WebPageDigger;
+            digger2.PrintPageData();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cs/Prototype.Pattern/WebPageDigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Prototype.Pattern
+{
+    class WebPageDigger : ICloneable
+    {
+        private string title;
+        private int headerTagCount;
+        private string firstHeaderTagContents;
+
+        public WebPageDigger(string url) 
+        {
+            var clien = new WebClient();
+            Dig(clien.DownloadString(url));
+
+        }
+        private void Dig(string page)
+        {
+            title = "foo title";
+            headerTagCount = 3;
+            firstHeaderTagContents = "bar header text";
+        }
+        public void PrintPageData()
+        {
+            Console.WriteLine("Title: {0},\nHeader Count: {1},\nFirst Header: {2}", title, headerTagCount, firstHeaderTagContents);
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
