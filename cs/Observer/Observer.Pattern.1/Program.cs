290e9c11145a0d302b6e38682816c33b9a1b4e9f
santiaago <santiago.ariassar@gmail.com>
2013-04-09 18:16:29 +0200

    Observer pattern part 1

---
+++ cs/Observer/Observer.Pattern.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer.Pattern._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Stock s in SampleData.getNext()) {
+                if (s.Symbol == "GOOG")
+                    Console.WriteLine("Google's price is: {0}", s.Price);
+                if(s.Symbol == "MSFT" && s.Price > 10.00m)
+                    Console.WriteLine("Microsoft has reached the target price: {0}", s.Price);
+                if(s.Symbol == "XOM")
+                    Console.WriteLine("Exxon mobile's price is: {0}", s.Price);
+            }
+        }
+    }
+}
