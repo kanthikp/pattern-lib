cf8414c611d1d6c848f4e707799ebeda910158f4
santiaago <santiago.ariassar@gmail.com>
2013-04-09 13:36:08 +0200

    Prototype pattern part 2

---
+++ cs/Prototype/Prototype.Pattern.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype.Pattern._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var puzzle = new LogicPuzzle();
+            puzzle.SwitchBlueSwitch();
+            puzzle.ToggleBigToggle();
+            puzzle.SwitchBlueSwitch();
+            puzzle.ToggleBigToggle();
+            puzzle.SwitchRedSwitch();
+
+            puzzle.PrintState();
+
+            var puzzle2 = puzzle.Clone() as LogicPuzzle;
+            puzzle2.PrintState();
+
+            Console.ReadKey();
+        }
+    }
+}
