d263d68474b2bdcc450cc53f50a48fdabaece00d
santiaago <santiago.ariassar@gmail.com>
2013-04-09 14:59:18 +0200

    Prototype pattern part 3

---
+++ cs/Prototype.Pattern.3/ComplicatedUnstableObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype.Pattern._3
+{
+    public class ComplicatedUnstableObject: ICloneable
+    {
+        internal ComplicatedUnstableObject(string a, int b, int c, char d, string e)
+        { }
+        internal ComplicatedUnstableObject(int ba, int c, char d, string e)
+        { }
+        internal ComplicatedUnstableObject(string a, char da, string c)
+        { }
+        internal ComplicatedUnstableObject(string a, int c, char d, string e, string f)
+        { }
+        //only expose one simple constructor for your API and do the complex stuff inside.
+        public static ComplicatedUnstableObject Reference()
+        {
+            return new ComplicatedUnstableObject("a", 7, 34, 'c', "st");
+        }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ cs/Prototype.Pattern.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype.Pattern._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var complicatedObject = ComplicatedUnstableObject.Reference(); ; //new ComplicatedUnstableObject can't do new here from the outside
+            var complicatedObject2 = complicatedObject.Clone();
+        }
+    }
+}
