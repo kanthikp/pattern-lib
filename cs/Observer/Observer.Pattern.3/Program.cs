ec98bba7f2d54413233fd9463b8325608de36121
santiaago <santiago.ariassar@gmail.com>
2013-04-11 13:29:48 +0200

    Observer patter part 4 IObservable and IObserver

---
+++ cs/Observer/Observer.Pattern.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer.Pattern._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockTicker st = new StockTicker();
+
+            GoogleMonitor gf = new GoogleMonitor();
+            MicrosoftMonitor mf = new MicrosoftMonitor();
+
+            using (st.Subscribe(gf))
+            using (st.Subscribe(mf))
+            {
+                foreach (var s in SampleData.getNext())
+                {
+                    st.Stock = s;
+                }
+            }
+        }
+    }
+}
