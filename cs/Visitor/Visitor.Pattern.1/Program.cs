98bb6e2f48dfc780d0b61b2764171e47a8c12a7a
santiaago <santiago.ariassar@gmail.com>
2013-04-30 17:12:24 +0200

    visitor pattern part 2

--- cs/Visitor/Visitor.Pattern.1/Program.cs
+++ cs/Visitor/Visitor.Pattern.1/Program.cs
@@ -10,20 +10,15 @@ namespace Visitor.Pattern._1
         static void Main(string[] args)
         {
             var person = new Person();
-            person.BankAccounts.Add(new BankAccount { Amount = 1000, MonthlyInterest = 0.01});
-            person.BankAccounts.Add(new BankAccount { Amount = 2000, MonthlyInterest = 0.02});
-            person.RealEstates.Add(new RealEstate { EstimatedValue = 79000, MonthlyRent = 500 });
-            person.Loans.Add(new Loan { Owed = 40000, MonthlyPayment = 40 });
+            person.Assets.Add(new BankAccount { Amount = 1000, MonthlyInterest = 0.01});
+            person.Assets.Add(new BankAccount { Amount = 2000, MonthlyInterest = 0.02 });
+            person.Assets.Add(new RealEstate { EstimatedValue = 79000, MonthlyRent = 500 });
+            person.Assets.Add(new Loan { Owed = 40000, MonthlyPayment = 40 });
 
-            int netWorth = 0;
-            foreach (var bankAccount in person.BankAccounts)
-                netWorth += bankAccount.Amount;
-            foreach (var realEstate in person.RealEstates)
-                netWorth += realEstate.EstimatedValue;
-            foreach (var loan in person.Loans)
-                netWorth -= loan.Owed;
+            var netWorthVisitor = new NetWorthVisitor();
+            person.Accept(netWorthVisitor);
 
-            Console.WriteLine(netWorth);
+            Console.WriteLine(netWorthVisitor.Total);
             Console.ReadLine();
         }
     }
--- cs/Visitor/Visitor.Pattern.1/Visitor.cs
+++ cs/Visitor/Visitor.Pattern.1/Visitor.cs
@@ -5,25 +5,73 @@ using System.Text;
 
 namespace Visitor.Pattern._1
 {
-    public class Person
+    public interface IVisitor
     {
-        public List<RealEstate> RealEstates = new List<RealEstate>();
-        public List<BankAccount> BankAccounts = new List<BankAccount>();
-        public List<Loan> Loans = new List<Loan>();
+        void Visit(RealEstate realEstate);
+        void Visit(BankAccount bankAccount);
+        void Visit(Loan loan);
     }
-    public class Loan
+    public interface IAsset
+    {
+        void Accept(IVisitor visitor);
+    }
+    public class NetWorthVisitor : IVisitor 
+    {
+        public int Total { get; set; }
+
+        public void Visit(RealEstate realEstate)
+        {
+            Total += realEstate.EstimatedValue;
+        }
+
+        public void Visit(BankAccount bankAccount)
+        {
+            Total += bankAccount.Amount;
+        }
+
+        public void Visit(Loan loan)
+        {
+            Total -= loan.Owed;
+        }
+    }
+    public class Person: IAsset
+    {
+        public List<IAsset> Assets= new List<IAsset>();
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach (var asset in Assets)
+                asset.Accept(visitor);
+        }
+    }
+    public class Loan : IAsset
     {
         public int Owed { get; set; }
         public int MonthlyPayment { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
     }
-    public class BankAccount
+    public class BankAccount : IAsset
     {
         public int Amount { get; set; }
         public double MonthlyInterest { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
     }
-    public class RealEstate
+    public class RealEstate: IAsset
     {
         public int EstimatedValue { get; set; }
         public int MonthlyRent { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
     }
 }
