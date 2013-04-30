a95a7b0098b21324ba981c76cd363315acb44ac7
santiaago <santiago.ariassar@gmail.com>
2013-04-30 17:01:04 +0200

    visitor pattern part 1

---
+++ cs/Visitor/Visitor.Pattern.1/Visitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Visitor.Pattern._1
+{
+    public class Person
+    {
+        public List<RealEstate> RealEstates = new List<RealEstate>();
+        public List<BankAccount> BankAccounts = new List<BankAccount>();
+        public List<Loan> Loans = new List<Loan>();
+    }
+    public class Loan
+    {
+        public int Owed { get; set; }
+        public int MonthlyPayment { get; set; }
+    }
+    public class BankAccount
+    {
+        public int Amount { get; set; }
+        public double MonthlyInterest { get; set; }
+    }
+    public class RealEstate
+    {
+        public int EstimatedValue { get; set; }
+        public int MonthlyRent { get; set; }
+    }
+}
