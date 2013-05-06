bc4517c47792aab383bc65c77e718252af179bdf
santiaago <santiago.ariassar@gmail.com>
2013-05-06 14:06:32 +0200

    Command Pattern part 1

---
+++ cs/Command/Command.Pattern.1/CommandExecutor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Command.Pattern._1
+{
+    public class CommandExecutor
+    {
+        internal void ExecuteCommand(string[] args)
+        {
+            switch (args[0])
+            { 
+                case "UpdateQuantity":
+                    UpdateQuantity(int.Parse(args[1]));
+                    break;
+                case "CreateOrder":
+                    CreateOrder();
+                    break;
+                case "ShipOrder":
+                    ShipOrder();
+                    break;
+                default:
+                    Console.WriteLine("Unrecognized command");
+                    break;
+            }
+        }
+
+        private void UpdateQuantity(int 
+            newQuantity)
+        {
+            const int oldQuantity = 5;
+            Console.WriteLine("DATABASE: Updated");
+
+            Console.WriteLine("LOG: Updated order quantity from {0} to {1}",oldQuantity,newQuantity);
+        }
+        private void ShipOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs/Command/Command.Pattern.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Command.Pattern._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintUsage();
+                return;
+            }
+            var processor = new CommandExecutor();
+            processor.ExecuteCommand(args);
+        }
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage: LoggingDemo CommandName Arguments");
+            Console.WriteLine("Commands:");
+            Console.WriteLine("     UpdateQuantity number");
+            Console.WriteLine("     CreateOrder");
+            Console.WriteLine("     ShipOrder");
+        }
+    }
+}
